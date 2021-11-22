b858283a49c51c3f3e9280d1d33e949bd6b24834
sanjidtk <sanjidmon@gmail.com>
2021-11-22 20:26:12 +0530

    fourth change commit

--- app/src/main/java/com/example/project_comfort_01/fragment1.java
+++ app/src/main/java/com/example/project_comfort_01/fragment1.java
@@ -4,17 +4,114 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-public class fragment1 extends Fragment {
+public class fragment1 extends Fragment implements  View.OnClickListener{
 
-
+    Button b1, b2 ,b3, b4, b5 ,b6, b7;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_front, container, false);
+
+       View v= inflater.inflate(R.layout.fragment_front, container, false);
+
+        b1= v.findViewById(R.id.btn1);
+        b2= v.findViewById(R.id.btn2);
+        b3= v.findViewById(R.id.btn3);
+        b4= v.findViewById(R.id.btn4);
+        b5= v.findViewById(R.id.btn5);
+        b6= v.findViewById(R.id.btn6);
+        b7= v.findViewById(R.id.btn7);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+
+
+        return v;
     }
+
+    public void defaultColour()
+    {
+        b1.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        b2.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        b3.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        b4.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        b5.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        b6.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        b7.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+    }
+    @Override
+    public void onClick(View v1) {
+        defaultColour();
+        switch (v1.getId()){
+
+            // cases applied over different buttons
+            case R.id.btn1:
+                // Toast message appears when button pressed
+
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                break;
+
+            case R.id.btn2:
+
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b2.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                break;
+
+            case R.id.btn3:
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b2.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b3.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                break;
+            case R.id.btn4:
+                // Toast message appears when button pressed
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b2.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b3.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b4.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                break;
+
+            case R.id.btn5:
+
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b2.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b3.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b4.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b5.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                break;
+
+            case R.id.btn6:
+
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b2.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b3.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b4.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b5.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b6.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+
+                break;
+
+            case R.id.btn7:
+
+                b1.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b2.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b3.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b4.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b5.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b6.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                b7.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+                break;
+        }
+    }
+
 }
